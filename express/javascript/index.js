eda0ed3c4d71e238310e552e87fa9a091a740a85
Isak Einberg <45336755+einbergisak@users.noreply.github.com>
2025-04-10 16:51:47 +0200

    Add JS JSON endpoint

--- express/javascript/index.js
+++ express/javascript/index.js
@@ -15,6 +15,26 @@ app.get("/lipsum.txt", (req, res) => {
   });
 });
 
+// Use JSON parsing middleware
+app.use(express.json());
+
+app.post("/json", (req, res) => {
+  try {
+    // Receive and parse JSON
+    const data = req.body;
+
+    // Check that there is data, as an empty body is parsed as an empty object
+    if (!data || Object.keys(data).length === 0) {
+      throw new Error();
+    }
+
+    // Serialize and respond
+    res.json(data);
+  } catch (error) {
+    res.status(400).send("Invalid request");
+  }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
