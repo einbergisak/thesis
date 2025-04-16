ef9145d735ebb8c62dd31f0075661d060f00e146
Isak Einberg <45336755+einbergisak@users.noreply.github.com>
2025-04-16 13:39:55 +0200

    Update Swift EchoData

--- vapor/swift/Sources/App/EchoData.swift
+++ vapor/swift/Sources/App/EchoData.swift
@@ -2,5 +2,19 @@ import Foundation
 import Vapor
 
 struct EchoData: Codable, Content {
+    let id: Int
     let message: String
+    let innerDataList: [InnerData]
+}
+
+struct InnerData: Codable, Content {
+    let id: Int
+    let number: Double
+    let itemList: [ListItem]
+}
+
+struct ListItem: Codable, Content {
+    let id: Int
+    let boolean: Bool
+    let string: String
 }
