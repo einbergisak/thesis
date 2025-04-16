299c5f8b54a4575e3c911f35bca6e48f63f312e2
Isak Einberg <45336755+einbergisak@users.noreply.github.com>
2025-04-16 13:59:34 +0200

    Update Kotlin EchoData

--- ktor/kotlin/src/main/kotlin/EchoData.kt
+++ ktor/kotlin/src/main/kotlin/EchoData.kt
@@ -2,5 +2,21 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class EchoData(
-    val message: String
+    val id: Int,
+    val message: String,
+    val innerDataList: List<InnerData>
+)
+
+@Serializable
+data class InnerData(
+    val id: Int,
+    val number: Double,
+    val itemList: List<ListItem>
+)
+
+@Serializable
+data class ListItem(
+    val id: Int,
+    val boolean: Boolean,
+    val string: String,
 )
